6119bb318b669d1dd682ac3e17a1fdfef3d4e559
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2022-02-22 18:54:02 +0800

    kernel patch 5.13

--- build/proxmox/build7.sh
+++ build/proxmox/build7.sh
@@ -2,8 +2,8 @@
 set -e
 
 export PVE_KERNEL_BRANCH=pve-kernel-5.13
-export RELAX_INTEL_GIT_REPO="https://github.com/jamestutton/relax-intel-rmrr.git"
-export RELAX_PATCH="add-relaxable-rmrr-5_11.patch"
+export RELAX_INTEL_GIT_REPO="https://github.com/lelemka0/relax-intel-rmrr.git"
+export RELAX_PATCH="add-relaxable-rmrr-5_13.patch"
 export PROXMOX_PATCH="proxmox7.patch"
 
 ./build.sh
